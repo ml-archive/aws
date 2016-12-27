f416c89a40e3579a4456380a8bad7212a6583da5
Brett Toomey <brettcwx@gmail.com>
2016-12-26 16:53:02 -0800

    Fixed Linux tests.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "AWS",
     targets: [
+        Target(name: "AWS", dependencies: ["EC2", "S3", "Driver"]),
         Target(name: "EC2", dependencies: ["Driver"]),
         Target(name: "S3", dependencies: ["Driver"]),
         Target(name: "Driver")
