62a2ffe24ea14599a170d14ad8eb342422cb6884
Joe Smith <yasumoto7@gmail.com>
2017-08-02 17:32:44 -0700

    Swiftlint Whitespace clearing

--- Sources/AWSSignatureV4/AWSSignatureV4.swift
+++ Sources/AWSSignatureV4/AWSSignatureV4.swift
@@ -20,14 +20,14 @@ public struct AWSSignatureV4 {
         case post = "POST"
         case put = "PUT"
     }
-    
+
     let service: String
     let host: String
     let region: String
     let accessKey: String
     let secretKey: String
     let contentType = "application/x-www-form-urlencoded; charset=utf-8"
-    
+
     internal var unitTestDate: Date?
 
     var amzDate: String {
@@ -64,7 +64,7 @@ public struct AWSSignatureV4 {
             canonicalHash
         ].joined(separator: "\n")
     }
-    
+
     func getSignature(_ stringToSign: String) throws -> String {
         let dateHMAC = try HMAC(.sha256, dateStamp()).authenticate(key: "AWS4\(secretKey)")
         let regionHMAC = try HMAC(.sha256, region).authenticate(key: dateHMAC)
@@ -83,7 +83,7 @@ public struct AWSSignatureV4 {
             "aws4_request"
         ].joined(separator: "/")
     }
-    
+
     func getCanonicalRequest(
         payloadHash: String,
         method: Method,
@@ -98,7 +98,7 @@ public struct AWSSignatureV4 {
         // If this breaks for another API call then try to reintroduce
         // but remove both & and = from Byte.awsQueryAllowed
         //let query = try query.urlEncode(allowing: Byte.awsQueryAllowed)
-        
+
         return [
             method.rawValue,
             path,
@@ -132,17 +132,17 @@ extension AWSSignatureV4 {
             headers["x-amz-content-sha256"] = hash
         }
     }
-    
+
     func alphabetize(_ dict: [String : String]) -> [(key: String, value: String)] {
         return dict.sorted(by: { $0.0.lowercased() < $1.0.lowercased() })
     }
-    
+
     func createCanonicalHeaders(_ headers: [(key: String, value: String)]) -> String {
         return headers.map {
             "\($0.key.lowercased()):\($0.value)"
         }.joined(separator: "\n")
     }
-    
+
     func createAuthorizationHeader(
         algorithm: String,
         credentialScope: String,
@@ -177,11 +177,11 @@ extension AWSSignatureV4 {
         let algorithm = "AWS4-HMAC-SHA256"
         let credentialScope = getCredentialScope()
         let payloadHash = try payload.hashed()
-        
+
         var headers = headers
 
         generateHeadersToSign(headers: &headers, host: host, hash: payloadHash)
-        
+
         let sortedHeaders = alphabetize(headers)
         let signedHeaders = sortedHeaders.map { $0.key.lowercased() }.joined(separator: ";")
         let canonicalHeaders = createCanonicalHeaders(sortedHeaders)
@@ -224,12 +224,12 @@ extension AWSSignatureV4 {
             "Authorization": authorizationHeader,
             "Host": self.host
         ]
-      
+
         headers.forEach { key, value in
             let headerKey = HeaderKey(stringLiteral: key)
             requestHeaders[headerKey] = value
         }
-      
+
         return requestHeaders
     }
 }
