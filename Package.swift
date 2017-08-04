c9657d9ef9638f4c2f00202ee5403819429d5099
Joe Smith <yasumoto7@gmail.com>
2017-08-03 18:59:43 -0700

    Add AutoScaling to AWS target

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "AWS",
     targets: [
-        Target(name: "AWS", dependencies: ["EC2", "S3", "AWSSignatureV4"]),
+        Target(name: "AWS", dependencies: ["AutoScaling", "EC2", "S3", "AWSSignatureV4"]),
         Target(name: "EC2", dependencies: ["AWSSignatureV4"]),
         Target(name: "AutoScaling", dependencies: ["AWSSignatureV4"]),
         Target(name: "S3", dependencies: ["AWSSignatureV4"]),
